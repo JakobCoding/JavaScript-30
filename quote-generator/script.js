a7c51c4e4e53ed51a995af9727cf94cc984f950b
Jake Chalmers <jakechalmers86@gmail.com>
2025-10-31 19:56:25 +0800

    back up API options comments added

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -26,3 +26,14 @@ function newQuote() {
 // On Load
 // getQuotes();
 newQuote();
+
+// There is an alternative option for this project that does not require an API key - https://zenquotes.io/
+
+   // Sample Requests
+   // https://zenquotes.io/api/quotes - Generate a JSON array of 50 random quotes on each request
+     
+   // https://zenquotes.io/api/today - Generate the quote of the day on each request
+     
+   // https://zenquotes.io/api/random - Generate a random quote on each request
+
+
