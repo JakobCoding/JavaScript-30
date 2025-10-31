462a6a361fd772862790e9861fd7e099347b6b73
Jake Chalmers <jakechalmers86@gmail.com>
2025-10-31 19:53:37 +0800

    Local Quotes.js added to project files

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,26 +1,28 @@
 // Get Quotes from API
 // An asynchronous function can run at any time independently and it won't stop the browser from completing the loading of a page. 
 
-let apiQuotes = [];
+// let apiQuotes = [];
+
 
 // Show New Quote
 function newQuote() {
-    // Pick a Random quote from apiQuotes Array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Pick a Random quote from localQuotes Array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     console.log(quote);
 }
 
-async function getQuotes() {
-    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-        // console.log(apiQuotes[12]); picks 1 specific quote, we want random quotes generated every button click from array[]
-    }catch (error) {
-        // Catch Error Here
-    }
-}
+// async function getQuotes() {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//         // console.log(apiQuotes[12]); picks 1 specific quote, we want random quotes generated every button click from array[]
+//     }catch (error) {
+//         // Catch Error Here
+//     }
+// }
 
 // On Load
-getQuotes();
+// getQuotes();
+newQuote();
