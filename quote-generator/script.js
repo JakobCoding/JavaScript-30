a7f6ef95bfcedb50ef1ee21b18077d1597a0575e
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-02 09:55:15 +0800

    Cleaned up JS code, comments and funtion names for readability

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -5,37 +5,48 @@ const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById('loader');
 
+/*
+Get Quotes from API: 
+An asynchronous function can run at any time independently and it won't stop the browser from completing the loading of a page. 
 
-// Get Quotes from API
-// An asynchronous function can run at any time independently and it won't stop the browser from completing the loading of a page. 
+let apiQuotes = [];
 
-// let apiQuotes = [];
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+    
+    }
+}
+getQuotes();
+*/
 
-// Show Loading 
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true; 
 }
 
-// Show New Quote
+// Show New Quote From local Storage instead of API
 function newQuote() {
-    loading(); 
+    showLoadingSpinner(); 
     // Pick a Random quote from localQuotes Array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    // Check if author field is blank and replace it with quote unknown
-    // if statement shows if there is NO !quote.author to show "unkown" on the app underneath the quote.  
+
+    // Check if author field is blank and replace it with "Unknown" 
     if (!quote.author) {
         authorText.textContent = "Unknown"; 
     }   else {
         authorText.textContent = quote.author; 
     }
-    // Check the quote length to determin the sytling 
+    // Check the quote length to determin font size styling
     if (quote.text.length > 80) {
         quoteText.classList.add("long-quote");
     }   else {
@@ -43,11 +54,11 @@ function newQuote() {
     }
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text; 
-    complete(); 
+    removeLoadingSpinner(); 
 }
 
 
-// Post quote on X
+// Post quote on X.com
 function postQuote() {
     const xUrl = `https://x.com/intent/post?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(xUrl, '_blank');
@@ -57,30 +68,4 @@ function postQuote() {
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', postQuote);
 
-// async function getQuotes() {
-//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//         // console.log(apiQuotes[12]); picks 1 specific quote, we want random quotes generated every button click from array[]
-//     }catch (error) {
-//         // Catch Error Here
-//     }
-// }
-
-// On Load
-// getQuotes();
 newQuote();
-
-
-// There is an alternative option for this project that does not require an API key - https://zenquotes.io/
-
-   // Sample Requests
-   // https://zenquotes.io/api/quotes - Generate a JSON array of 50 random quotes on each request
-     
-   // https://zenquotes.io/api/today - Generate the quote of the day on each request
-     
-   // https://zenquotes.io/api/random - Generate a random quote on each request
-
-
