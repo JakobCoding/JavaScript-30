d571d98d4e21d7712fd2e2a3b869a82a6f234abf
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-03 19:14:55 +0800

    function displayPhotos element & attributes added

--- Infinite-Scroll/script.js
+++ Infinite-Scroll/script.js
@@ -3,6 +3,25 @@ const loader = document.getElementById("loader");
 
 let photosArray = [];
 
+// Create Elements For Links and Photos, Add to Dom
+function displayPhotos() {
+    // Run function for each object in photosArray 
+    photosArray.forEach((photo) => {
+      // Create <a> to link to Unsplash
+      const item = document.createElement('a');
+      item.setAttribute('href', photo.links.html);
+      item.setAttribute('target', '_blank');
+      // Create <img> for photo
+      const img = document.createElement('img');
+      img.setAttribute('src', photo.urls.regular);
+      img.setAttribute('alt', photo.alt_description);
+      img.setAttribute('title', photo.alt_description);
+      // Put <img> inside <a>, then put both inside imageContainer Element
+      item.appendChild(img);
+      imageContainer.appendChild(item);
+    });
+}
+
 // Unsplash API
 const count = 10; 
 const apiKey = 'aEV0CQiuScNbXZfKDhlmibMa6_vlGoK62VVhXAoAycA';
@@ -13,7 +32,7 @@ async function getPhotos () {
     try{
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
+        
     }   catch (error) {
         // Catch erro Here
     }
