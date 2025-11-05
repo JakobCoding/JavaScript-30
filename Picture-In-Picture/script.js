b04ff2e90ce7d54c4c5ac6d30c959d25b7b5ea46
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-05 10:48:23 +0800

    async function added to await media API

--- Picture-In-Picture/script.js
+++ Picture-In-Picture/script.js
@@ -4,7 +4,7 @@ const button = document.getElementById('button');
 // Promt to select media stream, pass to video element, then play 
 async function selectMediaStream() {
     try {
-        something
+        const mediastream = await navigator.mediaDevices.getDisplayMedia(); 
     } catch (error) {
         // catch erros here 
         console.log('Woops error here:', error);
